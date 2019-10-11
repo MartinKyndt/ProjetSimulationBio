27e37fc73784dc1b73899834d15a001e72517202
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2019-10-11 09:47:27 +0200

    Méthode pour load et ecrire les data

--- evolution.py
+++ evolution.py
@@ -1,3 +1,16 @@
+
 print('Ceci est notre code') 
 
 
+
+def loadData(TSS, TTS) : 
+	data = [] 
+	f1 = open(TSS)
+	f2 = open(TTS)
+	tss = [[e for e in l[:-1].split('\\')] for l in f1.readlines()]
+	tts = f2.readlines() 
+	print(tss)
+
+#def writeData(TSS, TTS)  
+
+loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
