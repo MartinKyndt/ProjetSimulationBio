ac3a7900fd5ec7347507c15f4724addafcada0bd
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2019-10-18 08:18:46 +0200

    Dernière version de méthode loadData qui marche

--- evolution.py
+++ evolution.py
@@ -10,7 +10,7 @@ def loadData(TSS, TTS) :
 	tss = [[e for e in l[:-1].split('\t')] for l in f1.readlines()[1:]]
 	tts = [[e for e in l[:-1].split('\t')] for l in f2.readlines()[1:]]
 	for i in range(10) :
-		data.append([int(tss[i][3]),int(tts[i][3]), (3000*i)+1, 3000*(i+1)]) 
+		data.append([int(tss[i][2]),int(tts[i][2]), (3000*i)+1, 3000*(i+1)]) 
 	print(data)
 
 #def writeData(TSS, TTS)  
