def89a69ae6438674fc58df722f301dfafe3efa8
Paco <paco.salomon@gmail.com>
2019-10-18 08:56:57 +0200

    Add method insertion

--- evolution.py
+++ evolution.py
@@ -11,8 +11,24 @@ def loadData(TSS, TTS) :
 	tts = [[e for e in l[:-1].split('\t')] for l in f2.readlines()[1:]]
 	for i in range(10) :
 		data.append([int(tss[i][2]),int(tts[i][2]), (3000*i)+1, 3000*(i+1)]) 
-	print(data)
+	return data
+	
 
-#def writeData(TSS, TTS)  
+#def writeData(TSS, TTS) 
 
-loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
+#Ajoute un codon à une position définie dans le génome
+#Décale toutes les positions suivantes
+def insertion(data, pos) :
+	for i in range(len(data)) :
+		for j in range(len(data[0])) :
+			if data[i][j] >= pos :
+				data[i][j] += 1
+ 
+
+data = loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
+print(data)
+#insertion(data, 3500)
+
+
+
+
