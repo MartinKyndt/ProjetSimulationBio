4401fa363114a5ed564a339b1c039783a6518fec
J.Antoine <justineantoine17@gmail.com>
2019-10-25 09:39:56 +0200

    Changement WriteData pour gene_pos

--- evolution.py
+++ evolution.py
@@ -15,8 +15,8 @@ def loadData(TSS, TTS) :
 	tss = [[e for e in l[:-1].split('\t')] for l in f1.readlines()[1:]]
 	tts = [[e for e in l[:-1].split('\t')] for l in f2.readlines()[1:]]
 	for i in range(10) :
-		gene_pos.append([(3000*i)+1, 3000*(i+1)])
-		dom_pos.append([int(tss[i][2]),int(tts[i][2])])
+		dom_pos.append([(3000*i)+1, 3000*(i+1)])
+		gene_pos.append([int(tss[i][2]),int(tts[i][2])])
 	return (np.array(gene_pos), np.array(dom_pos))
 
 
@@ -28,19 +28,19 @@ def writeData(gene_pos) :
 	for i in range(len(gene_pos)):
 		f1.write(str(i)+'\t')
 		f2.write(str(i)+'\t')
-		if gene_pos[i,1]<gene_pos[i,2]:
+		if gene_pos[i,0]<gene_pos[i,1]:
 			f1.write('+\t')
 			f2.write('+\t')
 		else:
 			f1.write('-\t')
 			f2.write('-\t')
-		f1.write(str(gene_pos[i,1])+'\t.2\n')
-		f2.write(str(gene_pos[i,2])+'\t1.\n')
+		f1.write(str(gene_pos[i,0])+'\t.2\n')
+		f2.write(str(gene_pos[i,1])+'\t1.\n')
 	f1.close()
 	f2.close()
 	
-data = loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
-writeData(data)
+gene_pos, dom_poss = loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
+writeData(gene_pos)
 
 #Ajoute un codon à une position définie dans le génome
 #Décale toutes les positions suivantes
@@ -54,9 +54,10 @@ def insertion(gene_pos, dom_pos, pos) :
 			if dom_pos[i][j] >= pos :
 				dom_pos[i][j] += 1
 
-
+ 
  #pos1 < pos2
  #Les positions ne se trouvent pas dans les régions codantes
+
 def inversion(data, pos1, pos2) :
 	data_copy = data
 	new_pos_gene = []
