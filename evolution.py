dde285c87a70e891fe7d589eaa68fff807c04f7f
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2019-11-08 07:51:22 +0100

    d

--- evolution.py
+++ evolution.py
@@ -11,12 +11,9 @@ def loadData(TSS, TTS) :
 	tts = [[e for e in l[:-1].split('\t')] for l in f2.readlines()[1:]]
 	for i in range(10) :
 		data.append([int(tss[i][2]),int(tts[i][2]), (3000*i)+1, 3000*(i+1)]) 
-<<<<<<< HEAD
 	return(data)
-=======
-	return data
-	
 
+	
 #def writeData(TSS, TTS) 
 
 #Ajoute un codon à une position définie dans le génome
@@ -26,39 +23,22 @@ def insertion(data, pos) :
 		for j in range(len(data[0])) :
 			if data[i][j] >= pos :
 				data[i][j] += 1
- 
 
-data = loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
-print(data)
-#insertion(data, 3500)
-
-
->>>>>>> def89a69ae6438674fc58df722f301dfafe3efa8
-
-
-<<<<<<< HEAD
+#Enlève un codon à une position définie dans le génome
+#Décale toutes les positions suivantes
 def deletion(data, pos) : 
 	for i in range(len(data)) : 
 		for j in range(len(data[0])) : 
 			if(data[i][j] > pos) : 
 				data[i][j] = data[i][j]-1 
 	
-def randomPos(data) : 
-	deb = 1 
-	fin = data[9][3]
-	count = 0
-	while(count != 10) : 
-		count = 0
-		pos = random.randint(deb,fin)
-		for i in range(len(data)): 
-			if(pos < data[i][0] & pos > data[i][1]) : 
-				count +=1 
-	return pos
+#Génère une position aléatoire dans le génome, en dehors des gènes
+def randomPos(data) 
 
+
+#TEST
 dat = loadData('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat') 
 print(dat)
 deletion(dat, 3) 
 print(dat) 
 print(randomPos(dat))
-=======
->>>>>>> def89a69ae6438674fc58df722f301dfafe3efa8
