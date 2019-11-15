a8183b5b19ed2ae2709eb6e3e9ac5ade72c6fe7e
Kyndt Martin <martin.kyndt@insa-lyon.fr>
2019-11-15 09:41:03 +0100

    Ajout de fitness dans evolution

--- evolution.py
+++ evolution.py
@@ -172,6 +172,23 @@ def randomPos(dom_pos, gene_pos) :
 	print("position for inversion", pos)
 	return pos
 
+def fitness(result, expected) : 
+	obs = [] 
+	cible = [] 
+	f1 = open(result)
+	f2 = open(expected)
+	t = [[e for e in l[:-1].split()] for l in f1.readlines()[0:]]
+	tt = [[e for e in l[:-1].split()] for l in f2.readlines()[0:]] 
+	for i in range(10) :
+		obs.append(int(t[i][4]))
+		cible.append(int(tt[i][4]))
+	
+	fitness = 0 
+	for i in range(10) : 
+		fitness += math.log(obs[i]/cible[i])
+	
+	return(fitness)
+	
 
 
 writeData_init('tousgenesidentiques/TSS.dat', 'tousgenesidentiques/TTS.dat')
